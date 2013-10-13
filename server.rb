7486c9f5ef7229db812e5a92277de726dce8bce4
Jonny Arnold <jonny.arnold89@gmail.com>
2013-10-13 19:17:46 +0100

    Fixed mixing of tabs and spaces

--- server.rb
+++ server.rb
@@ -4,10 +4,10 @@ require './sayings'
 set :static_cache_control, [:public, max_age: 60 * 60 * 24]
 
 get '/' do
-	redirect '/index.html'
+  redirect '/index.html'
 end
 
 get '/saying' do
-	sayings = SayingSplitter.new(File.read('sayings.txt'))
+  sayings = SayingSplitter.new(File.read('sayings.txt'))
   sayings.get_saying
 end
