b92e05330ca3eac4e9b1f4d74d754cd571331bba
Jonny Arnold <jonny.arnold@tessella.com>
2013-12-06 18:00:37 +0000

    Syntax fix (I hate caching...)

--- public/app.js
+++ public/app.js
@@ -10,49 +10,48 @@ ga('send', 'pageview');
 // Changes the background from AJAX call
 function changeBackground()
 {
-    if ($('#curtain').css('opacity') == 0) {
-      $.get('/bg', function(bg) {
-        var newBackground = bg;
+  if ($('#curtain').css('opacity') == 0) {
+    $.get('/bg', function(bg) {
+      var newBackground = bg;
 
-        $('#curtain').animate({'opacity': 1}, 500, function() {
-          $('body').css({'background': '#000 url('+newBackground+') no-repeat center', 'background-size': 'cover'});
-          $('#saying').animate({'opacity' : 1}, 500);
-            });
-        });
-      }
-		
-	} else {
-		$('#saying').animate({'opacity' : 1}, 500);
-	}
+      $('#curtain').animate({'opacity': 1}, 500, function() {
+        $('body').css({'background': '#000 url('+newBackground+') no-repeat center', 'background-size': 'cover'});
+        $('#saying').animate({'opacity' : 1}, 500);
+          });
+    });
+  }
+  else {
+    $('#saying').animate({'opacity' : 1}, 500);
+  }
     $('#curtain').animate({'opacity': 0}, 500);
 }
 
 function get_saying() {
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open("GET", "/saying", false);
-	xmlHttp.send(null);
-	var fadeOutTime = 500;
-	
-	// Upon first run the animation cannot hold up the callback
-	if ($('#saying').css('opacity') == 0) {
-		fadeOutTime = 0;
-	}
-	
-	$('#saying').animate({'opacity' : 0}, fadeOutTime, function() {
-		$('#saying').html(xmlHttp.responseText);
-		$('#tweet').attr('href', generate_tweet_url(xmlHttp.responseText))
-	});
-	
+  var xmlHttp = new XMLHttpRequest();
+  xmlHttp.open("GET", "/saying", false);
+  xmlHttp.send(null);
+  var fadeOutTime = 500;
+  
+  // Upon first run the animation cannot hold up the callback
+  if ($('#saying').css('opacity') == 0) {
+    fadeOutTime = 0;
+  }
+  
+  $('#saying').animate({'opacity' : 0}, fadeOutTime, function() {
+    $('#saying').html(xmlHttp.responseText);
+    $('#tweet').attr('href', generate_tweet_url(xmlHttp.responseText))
+  });
+  
     changeBackground();
 }
 
 // Creates a Twitter Share URL for use on a Tweet button
 // as documented @ https://dev.twitter.com/docs/tweet-button
 function generate_tweet_url(saying) {
-	url = window.location.origin
-	tweet_text = "\"" + saying + "\" "; // Twitter adds URL automatically
+  url = window.location.origin
+  tweet_text = "\"" + saying + "\" "; // Twitter adds URL automatically
 
-	return "https://twitter.com/share" +
-		"?url=" + url +
-		"&text=" + encodeURIComponent(tweet_text)
+  return "https://twitter.com/share" +
+    "?url=" + url +
+    "&text=" + encodeURIComponent(tweet_text)
 }
